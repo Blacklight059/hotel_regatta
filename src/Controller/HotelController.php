55766c59f90434bac584b93355aa7b1264bd7b03
Blacklight059 <81182554+Blacklight059@users.noreply.github.com>
2024-03-20 17:45:05 +0100

    Created pages for hotel and restaurant

--- src/Controller/HotelController.php
+++ src/Controller/HotelController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController
+class HotelController extends AbstractController
 {
-    #[Route('/', name: 'Homepage')]
+    #[Route('/hotel', name: 'app_hotel')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('hotel/index.html.twig', [
+            'controller_name' => 'HotelController',
         ]);
     }
 }
--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController
+class RestaurantController extends AbstractController
 {
-    #[Route('/', name: 'Homepage')]
+    #[Route('/restaurant', name: 'app_restaurant')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('restaurant/index.html.twig', [
+            'controller_name' => 'RestaurantController',
         ]);
     }
 }
