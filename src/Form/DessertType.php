e2a558f05643ec8a37c2cf9395bbb6e6b735bd53
Blacklight059 <81182554+Blacklight059@users.noreply.github.com>
2024-03-24 21:45:31 +0100

    Added style and updated form

--- src/Form/DessertType.php
+++ src/Form/DessertType.php
@@ -6,20 +6,39 @@ use App\Entity\Dessert;
 use App\Entity\Menu;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class DessertType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('price')
-            ->add('menus', EntityType::class, [
-                'class' => Menu::class,
-                'choice_label' => 'id',
-                'multiple' => true,
+            ->add('name', TextType::class, [
+                'required' => true, 
+                'label' => 'Titre',
+                'constraints' => [
+                    new NotBlank([
+                    'message' => "Veuillez saisir un nom"
+                    ]),
+                    new Length([
+                    'min' => 6,
+                    'minMessage' => "Le titre doit contenir au minimum {{ limit }} caractères"
+                    ]),
+                ],
+                'row_attr' => [
+                    'class' => 'm-3',
+                ],
+            ])            
+            ->add('price', MoneyType::class, [
+                'label' => 'Prix',
+                'row_attr' => [
+                    'class' => 'm-3',
+                ],
             ])
         ;
     }
--- src/Form/MenuType.php
+++ src/Form/MenuType.php
@@ -8,16 +8,40 @@ use App\Entity\Menu;
 use App\Entity\Starter;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class MenuType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('price')
+        ->add('name', TextType::class, [
+            'required' => true, 
+            'label' => 'Titre',
+            'constraints' => [
+                new NotBlank([
+                'message' => "Veuillez saisir un nom"
+                ]),
+                new Length([
+                'min' => 6,
+                'minMessage' => "Le titre doit contenir au minimum {{ limit }} caractères"
+                ]),
+            ],
+            'row_attr' => [
+                'class' => 'm-3',
+            ],
+        ])            
+        ->add('price', MoneyType::class, [
+            'label' => 'Prix',
+            'row_attr' => [
+                'class' => 'm-3',
+            ],
+        ])
             ->add('starter', EntityType::class, [
                 'class' => Starter::class,
                 'choice_label' => 'id',
